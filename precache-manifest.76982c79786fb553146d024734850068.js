85adca2276898dde04213573e00134d4716338c6
Roman Zhydyk <roman@student.dei.uc.pt>
2020-12-29 22:38:11 +0000

    Updates

---
+++ precache-manifest.76982c79786fb553146d024734850068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606cfca905709a742c45a412e6f9dd05",
+    "url": "/notify-client/index.html"
+  },
+  {
+    "revision": "defb15fb33f73e78880a",
+    "url": "/notify-client/static/js/2.9e875e50.chunk.js"
+  },
+  {
+    "revision": "934ae634abc1c4b282d83a2b3f70935a",
+    "url": "/notify-client/static/js/2.9e875e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc79b4b3564bc9e33855",
+    "url": "/notify-client/static/js/main.3833eaa1.chunk.js"
+  },
+  {
+    "revision": "f8ef8018d0ba41ae37e6",
+    "url": "/notify-client/static/js/runtime-main.c7017949.js"
+  },
+  {
+    "revision": "0d3121b4f60ec6632ba257df6ffad0fd",
+    "url": "/notify-client/static/media/notes.0d3121b4.svg"
+  }
+]);
