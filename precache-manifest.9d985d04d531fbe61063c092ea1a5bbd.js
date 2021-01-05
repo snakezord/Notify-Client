a7a8197e8cf3c1a3b7e50cfe45c1e00c544db074
Roman Zhydyk <roman@student.dei.uc.pt>
2021-01-05 16:57:35 +0000

    Updates

---
+++ precache-manifest.9d985d04d531fbe61063c092ea1a5bbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23824d75127f439ad65e290e3048ad58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8043d378ae230b451bc9",
+    "url": "/static/js/2.046a3af5.chunk.js"
+  },
+  {
+    "revision": "934ae634abc1c4b282d83a2b3f70935a",
+    "url": "/static/js/2.046a3af5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9214bc3b8c20140f9da",
+    "url": "/static/js/main.1950cec9.chunk.js"
+  },
+  {
+    "revision": "9b38937966e20fe5910f",
+    "url": "/static/js/runtime-main.25c7afd5.js"
+  },
+  {
+    "revision": "0d3121b4f60ec6632ba257df6ffad0fd",
+    "url": "/static/media/notes.0d3121b4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/notify-client/precache-manifest.9ceff3d57c3d23fe48cddb30541bee77.js"
+  "/precache-manifest.9d985d04d531fbe61063c092ea1a5bbd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/notify-client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
